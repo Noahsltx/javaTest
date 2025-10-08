07ff10f6601ab246130b08f048a1d3fcaad8d289
Noahsltx <Noah.Schmauss@gmail.com>
2025-10-08 08:30:00 +0200

    added constructor to animal

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         var Hund1 = new Tier(4, "Bello", 2, true);
         Hund1.macheSound("wau wau");
+        Hund1.bewegen();
     }
 }
